5c66b9b1f1c66e9233e20353abf423d60670b057
Oleari Filippo <oleari@gmail.com>
2021-12-07 15:17:04 +0100

    Get Parcheggi api funziona

--- WebAPI-Definitivo/Controllers/ParkingController.cs
+++ WebAPI-Definitivo/Controllers/ParkingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace WebAPI_Definitivo.Controllers
     public class ParkingController : ControllerBase
     {
         //Prendo tutti i parcheggi memorizzati
+        [Authorize]
         [HttpGet("/api/v1/parcheggio")]
         public ActionResult GetParking()
         {
