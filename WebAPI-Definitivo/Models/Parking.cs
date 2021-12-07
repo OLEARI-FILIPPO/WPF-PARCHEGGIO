276aa13b42bb5eb47c7d8a4501bb758102f286c4
Oleari Filippo <oleari@gmail.com>
2021-12-07 17:34:30 +0100

    Aggiunta veicolo finita

--- WebAPI-Definitivo/Models/Parking.cs
+++ WebAPI-Definitivo/Models/Parking.cs
@@ -9,6 +9,15 @@ namespace WebAPI_Definitivo.Models
 {
     public partial class Parking
     {
+        public Parking(string parkingId, bool stato, DateTime? entryTimeDate, long? vehicleId, long infoParkId)
+        {
+            ParkingId = parkingId;
+            Stato = stato;
+            EntryTimeDate = entryTimeDate;
+            VehicleId = vehicleId;
+            InfoParkId = infoParkId;
+        }
+
         public long Id { get; set; }
         public string ParkingId { get; set; }
         public bool Stato { get; set; }
