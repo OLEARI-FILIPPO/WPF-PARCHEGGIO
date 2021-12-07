cf47a261bf6776315ba44090cdacbb1f46719e8a
Oleari Filippo <oleari@gmail.com>
2021-12-07 11:42:28 +0100

    Modifiche con api get parcheggi

---
+++ WebAPI-Definitivo/Controllers/ParkingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Definitivo.Models;
+
+namespace WebAPI_Definitivo.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        //Prendo tutti i parcheggi memorizzati
+        [HttpGet("/api/v1/parcheggio")]
+        public ActionResult GetParking()
+        {
+            using(ParkingManagementContext model = new ParkingManagementContext())
+            {
+                List<Parking> parcheggi = model.Parking.ToList();
+                return Ok(parcheggi);
+            }
+        }
+        public ActionResult AddParking()
+        {
+            return Ok();
+        }
+    }
+}
