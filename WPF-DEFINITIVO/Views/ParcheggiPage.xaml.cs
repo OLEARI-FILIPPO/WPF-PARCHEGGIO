eee6a48dfcbc7f8a19d91b96e936aa864c6d8597
Alessandro Statuti <allestatuti02@gmail.com>
2021-12-09 11:07:05 +0100

    Commit

--- WPF-DEFINITIVO/Views/ParcheggiPage.xaml.cs
+++ WPF-DEFINITIVO/Views/ParcheggiPage.xaml.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
