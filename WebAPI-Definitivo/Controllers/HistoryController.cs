f36566ef764ae92e716a294a1537c5404149a24b
Alessandro Statuti <allestatuti02@gmail.com>
2021-12-12 15:45:51 +0100

    Update

--- WebAPI-Definitivo/Controllers/HistoryController.cs
+++ WebAPI-Definitivo/Controllers/HistoryController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebAPI_Definitivo.Model;
+using WebAPI_Definitivo.Models;
 
 namespace WebAPI_Definitivo.Controllers
 {
