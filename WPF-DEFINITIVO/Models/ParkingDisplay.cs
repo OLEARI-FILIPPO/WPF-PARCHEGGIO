11ca210d0b7794ea3a5f33f3ba314f4a66a8bc9b
Oleari Filippo <oleari@gmail.com>
2021-12-13 15:30:11 +0100

    Nome Parcheggi

---
+++ WPF-DEFINITIVO/Models/ParkingDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_DEFINITIVO.Models
+{
+    public class ParkingDisplay
+    {
+        public ParkingDisplay(string nomePosto, string stato, string nomeParcheggio)
+        {
+            NomePosto = nomePosto;
+            Stato = stato;
+            NomeParcheggio = nomeParcheggio;
+        }
+
+        public string NomePosto { get; set; }
+        public string Stato { get; set; }
+        public string NomeParcheggio { get; set; }
+    }
+}
